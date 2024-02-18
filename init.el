995300e2b5889acd138823f861c80d2a279c883d
0x736561 <dfern3321@gmail.com>
2024-02-18 00:54:20 -0300

    Primeiro Commit

---
+++ init.el
@@ -0,0 +1,18 @@
+(org-babel-load-file
+ (expand-file-name
+  "readme.org"
+  user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(no-littering telefone-line which-key telephone-line magit gruvbox-theme company)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(minibuffer-prompt ((t (:background "#443f49" :foreground "#cfa161" :weight bold))))
+ '(mode-line-highlight ((t (:foreground "#443f49")))))
